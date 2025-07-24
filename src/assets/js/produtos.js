04bb0b07ed598a32e76b243b18367eef533b423e
kauangomesm <kayangomes849@gmail.com>
2025-07-24 17:13:45 -0300

    Pagina Produtos Implementada

---
+++ src/assets/js/produtos.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const card = document.getElementById('produtos')
+    const cards = document.querySelectorAll('#produtos .card');
+    const vermais = document.getElementById('verMais');
+
+    card.innerHTML = ``
+
+    fetch('http://localhost:5500/src/produtos.json')
+    .then(res => res.json())
+    .then(data => {
+        const produtos = data;
+        for (let i = 0; i < produtos.length; i++) {
+            let produto = produtos[i];
+            let strTexto = `
+                <a href="produto.html?id=${produto.id}" class="card">
+                <img class="card-img-top" src="../${produto.imagem}">
+                <div class="card-body">
+                    <p class="card-text">${produto.nome}</p>
+                </div>
+            </a>
+            `;
+            card.innerHTML += strTexto;
+        }
+
+        mostrarCards();
+    });
+
+
+
+
+    let cardVisivel = 8;
+
+    function mostrarCards(animar = false) {
+        const cards = document.querySelectorAll('#produtos .card');
+        cards.forEach((card, idx) => {
+            if (idx < cardVisivel) {
+                card.style.display = 'block';
+                if (animar && idx >= cardVisivel - 8) {
+                    card.classList.add('fade-in');
+                    setTimeout(() => card.classList.remove('fade-in'), 500);
+                }
+            } else {
+                card.style.display = 'none';
+            }
+        });
+        if (cardVisivel >= cards.length) {
+            vermais.style.display = 'none';
+        }
+    }
+
+
+    vermais.addEventListener('click', function() {
+        cardVisivel += 8;
+        mostrarCards(true);
+    });
+});
